abef1b1e38ed84b7d57d8448d4f7496740a1d54d
antonipijuan <Nito@iMac-de-Antoni.home>
2018-06-03 20:50:13 +0200

    Seccion 12 terminada - Backend Server Terminado

---
+++ README.md
@@ -0,0 +1,3 @@
+# Backend-Server
+
+Este es el codigo necesario para establecer el backend conectado a Mongo usando Mongoose
--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,4 @@
 var express = require('express');
-
 var bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
 
@@ -8,6 +7,128 @@ var SEED = require('../config/config').SEED;
 var app = express();
 var Usuario = require('../models/usuario');
 
+const CLIENT_ID = require('../config/config').CLIENT_ID;
+
+
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client(CLIENT_ID);
+
+
+
+// ==========================================
+//  Autenticación De Google
+// ==========================================
+
+async function verify(token) {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: CLIENT_ID, // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+
+    const payload = ticket.getPayload();
+    //const userid = payload['sub'];
+    // If request specified a G Suite domain:
+    //const domain = payload['hd'];
+    return {
+        nombre: payload.name,
+        email: payload.email,
+        img: payload.picture,
+        google: true
+    }
+}
+
+
+app.post('/google', async(req, res) => {
+
+
+    var token = req.body.token;
+
+    var googleUser = await verify(token)
+        .catch(e => {
+            return res.status(403).json({
+                ok: false,
+                mensaje: 'Token no valido!!'
+            });
+        });
+
+    // return res.status(200).json({
+    //     ok: true,
+    //     mensaje: 'OK!!',
+    //     googleUser: googleUser
+    // });
+
+    Usuario.findOne({ email: googleUser.email }, (err, usuarioDB) => {
+
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar usuario',
+                errors: err
+            });
+        }
+
+        if (usuarioDB) {
+            if (usuarioDB.google === false) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'Debe de usar su autenticacion normal'
+
+                });
+            } else {
+                // Crear un token!!!
+                //usuarioDB.password = ':)';
+
+                var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 }); // 4 horas
+
+                res.status(200).json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token: token,
+                    id: usuarioDB._id
+                });
+
+            } // fi del else
+        } else {
+            // El usuario no existe, hay que crearlo
+            var usuario = new Usuario();
+
+            usuario.nombre = googleUser.nombre;
+            usuario.email = googleUser.email;
+            usuario.img = googleUser.img;
+            usuario.password = '-)';
+            usuario.google = true;
+
+            usuario.save((err, usuarioDB) => {
+
+                var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 }); // 4 horas
+
+                res.status(200).json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token: token,
+                    id: usuarioDB._id
+                });
+
+
+            });
+
+        }
+
+
+    }); // fi de findOne
+
+
+
+})
+
+
+
+
+// ==========================================
+//  Autenticación normal
+// ==========================================
 app.post('/', (req, res) => {
 
     var body = req.body;
@@ -17,14 +138,15 @@ app.post('/', (req, res) => {
         if (err) {
             return res.status(500).json({
                 ok: false,
-                mensaje: 'Error al buscar el usuario',
+                mensaje: 'Error al buscar usuario',
                 errors: err
             });
         }
+
         if (!usuarioDB) {
             return res.status(400).json({
                 ok: false,
-                mensaje: 'Credenciales incorrectas - mail',
+                mensaje: 'Credenciales incorrectas - email',
                 errors: err
             });
         }
@@ -37,27 +159,18 @@ app.post('/', (req, res) => {
             });
         }
 
-        // Crear un token
-        usuarioDB.password = ':-)';
-        var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 });
+        // Crear un token!!!
+        usuarioDB.password = ':)';
 
-
-
+        var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 }); // 4 horas
 
         res.status(200).json({
             ok: true,
-            mensaje: 'Login correcto',
             usuario: usuarioDB,
             token: token,
-            id: usuarioDB._id,
-            body: body
+            id: usuarioDB._id
         });
 
     });
-
-
-
 });
-
-
 module.exports = app;
